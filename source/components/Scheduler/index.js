64ec11f0b4f3dd1647c6fbcd2efea7b4809bfbcf
Andrew <ratnikov.am@gmail.com>
2018-11-03 12:31:19 +0200

    markup task manager

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -5,6 +5,10 @@ import React, { Component } from "react";
 import Styles from "./styles.m.css";
 import { api } from "../../REST"; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
+// Components
+import Checkbox from "../../theme/assets/Checkbox";
+import Task from "../Task";
+
 export default class Scheduler extends Component {
     render () {
         return (
@@ -19,7 +23,16 @@ export default class Scheduler extends Component {
                             <input placeholder = 'Type new task' type = 'text' />
                             <button>Add task</button>
                         </form>
+                        <ul>
+                            <Task />
+                        </ul>
                     </section>
+                    <footer>
+                        <Checkbox color1 = '#363636' color2 = '#fff' />
+                        <span className = { Styles.completeAllTasks }>
+                            Complete all tasks
+                        </span>
+                    </footer>
                 </main>
             </section>
         );
