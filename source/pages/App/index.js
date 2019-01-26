944caf926ed7b5fb67116ccb6a784748454b29bf
Andrew <ratnikov.am@gmail.com>
2019-01-26 15:33:08 +0200

    add store to app

--- source/pages/App/index.js
+++ source/pages/App/index.js
@@ -1,6 +1,10 @@
 // Core
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
+import { Provider } from "mobx-react";
+
+// Instruments
+import { tasksStore } from "../../store";
 
 // Components
 import Scheduler from "../../components/Scheduler";
@@ -8,6 +12,10 @@ import Scheduler from "../../components/Scheduler";
 @hot(module)
 export default class App extends Component {
     render () {
-        return <Scheduler />;
+        return (
+            <Provider tasksStore = { tasksStore }>
+                <Scheduler />
+            </Provider>
+        );
     }
 }
